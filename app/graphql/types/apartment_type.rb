54b41521df6d17a57be1a5d20d0b1cef12747e03
lijin1991-toptal <lijin1991@aol.com>
2020-10-26 19:21:23 +0800

    added field type

--- app/graphql/types/apartment_type.rb
+++ app/graphql/types/apartment_type.rb
@@ -2,6 +2,11 @@ module Types
   class ApartmentType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: true
+    field :price, Integer, null: true
+    field :sqm, Integer, null: true
+    field :number_of_bedrooms, Integer, null: true
+    field :number_of_bathrooms, Integer, null: true
+    field :picture, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
