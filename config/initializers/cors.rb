fc04edc528c4bda996030065bedebc7d5d746de9
lijin1991-toptal <lijin1991@aol.com>
2020-10-28 10:25:02 +0800

    fixed invalid authentication token issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      'http://localhost:3000', # local client
+      'http://localhost:8080', # local client
     )
 
     resource '*',
