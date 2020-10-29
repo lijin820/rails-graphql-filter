34d12dd0d5aee3519007948ebecb9aa1b5c0986b
lijin1991-toptal <lijin1991@aol.com>
2020-10-30 01:32:19 +0800

    added model validation

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Apartment < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :sqm, presence: true
+  validates :number_of_bedrooms, presence: true
+  validates :number_of_bathrooms, presence: true
 end
